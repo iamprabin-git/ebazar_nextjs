877fd0b13f811a33330c73b084c575d0ef6f43c4
Prabin Dangol <dprabin23@gmail.com>
2025-05-31 16:49:21 +0545

    quantity add
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

--- src/components/orders/Card.js
+++ src/components/orders/Card.js
@@ -1,31 +1,38 @@
 import Image from "next/image";
 
-function OrderItemCard({ product }) {
-    return (
-        <div className="border rounded m-2 p-2">
-            <Image
-                src={product.imageUrls[0]}
-                alt={product.name}
-                width={50}
-                height={50}
-                className="rounded-full"
-            />
-            <p>{product.name}</p>
-            <p>Quantity: {product.quantity}</p>
-            <p>Price: {product.price}</p>
-        </div>
-    );
+function OrderItemCard({ product, quantity }) {
+  return (
+    <div className="flex items-center border rounded m-2 p-2 gap-5">
+      <Image
+        src={product.imageUrls[0]}
+        alt={product.name}
+        width={50}
+        height={50}
+        className="rounded-full"
+      />
+      <div>
+      <p className="font-semibold">{product.name}</p>
+      <p className="text-sm">Price: Rs. {product.price} * {quantity}</p>
+    </div>
+    </div>
+  );
 }
 
 function OrderCard({ order }) {
-    return (
-        <div className="bg-gray-100 p-4 border rounded-lg my-2 dark:bg-gray-800 ">
-           <h2 className="text-lg font-semibold">Order Number: {order.orderNumber}</h2>
-           {order.orderItems.map((item, index) => (
-            <OrderItemCard key={index} product={item.product} />
-          ))}
-        </div>
-    );
+  return (
+    <div className="bg-gray-100 p-4 border rounded-lg my-2 dark:bg-gray-800 ">
+      <div className="flex items-center justify-between mb-2">
+        <h2 className="text-lg font-semibold"># {order.orderNumber}</h2>
+      <span className="bg-blue-100 text-blue-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-sm dark:bg-blue-900 dark:text-blue-300 uppercase">{order.status}</span>
+
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {order.orderItems.map((item, index) => (
+          <OrderItemCard key={index} product={item.product} quantity={item.quantity}/>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default OrderCard;
