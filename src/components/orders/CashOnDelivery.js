54fab1066a5484182b871752806973ecd3091a5c
Prabin Dangol <dprabin23@gmail.com>
2025-06-02 08:47:55 +0545

    cash on delivery
    Signed-off-by: Prabin Dangol <dprabin23@gmail.com>

---
+++ src/components/orders/CashOnDelivery.js
@@ -0,0 +1,55 @@
+"use client";
+import Image from "next/image";
+import cod from "../../assets/images/cod.png";
+import Spinner from "../products/Spinner";
+import { toast } from "react-toastify";
+import { confirmOrder } from "@/api/orders";
+import { useState } from "react";
+
+function CashOnDelivery({order}) {
+  const [loading, setLoading] = useState(false);
+
+  function confirmPayment() {
+    const orderId = order._id;
+
+    setLoading(true);
+    confirmOrder(orderId, {
+      status: "completed",
+      transactionId: crypto.randomUUID(),
+      paymentMethod: "cash",
+    })
+      .then((response) => {
+        toast.success("Order Completed Successfully", { autoClose: 750 });
+      })
+      .catch((error) => toast.error(error.response?.data, { autoClose: 750 }))
+      .finally(() => setLoading(false));
+  }
+
+  return (
+    <div className="flex items-center justify-between bg-slate-300 py-3 px-6 rounded-b-lg gap-3 dark:bg-slate-900">
+     <button
+        onClick={confirmPayment}
+        type="button"
+        disabled={loading}
+        className="flex gap-2 items-center justify-between cursor-pointer text-white bg-[#0b762f] hover:opacity-80 focus:ring-4 font-medium rounded-lg text-lg px-5 py-1 dark:bg-green-600 dark:hover:opacity-90 disabled:opacity-50 disabled:cursor-not-allowed"
+      >
+        {loading ? (
+          <Spinner className="h-5 w-5" />
+        ) : (
+          <Image
+            src={cod}
+            alt="cod"
+            width={1000}
+            height={1000}
+            className="h-10 w-10"
+          />
+        )}
+      Cash on Delivery
+      </button>
+      
+    </div>
+  )
+}
+
+export default CashOnDelivery;
+
--- src/components/orders/Confirm.js
+++ src/components/orders/Confirm.js
@@ -1,44 +1,38 @@
 "use client";
 import Image from "next/image";
-import khalti from "../../assets/images/khalti-white.png";
 import { checkoutOrder } from "@/api/orders";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import config from "@/config";
 import Spinner from "../products/Spinner";
+import KhaltiPayment from "./KhaltiPayment";
+import CashOnDelivery from "./CashOnDelivery";
 
-function ConfirmOrder({order}) {
 
+function ConfirmOrder({ order }) {
   const [loading, setLoading] = useState(false);
   function initiatePayment() {
-    const orderId= order._id;
+    const orderId = order._id;
 
     setLoading(true);
     checkoutOrder(orderId, {
-       returnUrl:`${config.appUrl}/orders/${orderId}/payment`,
-       websiteUrl:config.appUrl,
-      })
+      returnUrl: `${config.appUrl}/orders/${orderId}/payment`,
+      websiteUrl: config.appUrl,
+    })
       .then((response) => {
         const data = response?.data;
-        
+
         window.location.href = data.payment_url;
       })
       .catch((error) => toast.error(error.response?.data, { autoClose: 750 }))
       .finally(() => setLoading(false));
   }
   return (
-   <button
-   onClick={initiatePayment}
-          type="button"
-          disabled={loading}
-
-          className="flex gap-2 items-center justify-between cursor-pointer text-white bg-[#5E338D] hover:opacity-80 focus:ring-4 font-medium rounded-lg text-lg px-5 py-1 dark:bg-purple-600 dark:hover:opacity-90 disabled:opacity-50 disabled:cursor-not-allowed"
-        >
-          {loading ? <Spinner className="h-5 w-5"/>:
-          <Image src={khalti} alt="khalti" width={500} height={500} className="h-10 w-10"/>}
-          Pay Via Khalti
-        </button>
-  )
+    <div className="flex items-center justify-between bg-slate-300 py-3 px-6 rounded-b-lg gap-3 dark:bg-slate-900">
+     <CashOnDelivery order={order} />
+      <KhaltiPayment order={order} />
+    </div>
+  );
 }
 
 export default ConfirmOrder;
